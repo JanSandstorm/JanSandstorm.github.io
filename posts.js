347be299dd1d06b4873864d45e026b2c3ba37e8b
JanSandstorm <137768173+JanSandstorm@users.noreply.github.com>
2025-10-13 11:39:09 +0200

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,25 @@
+// posts.js — a shared list of all your pages in reverse chronological order
+const posts = [
+  	"index.html",              // The landing page (acts as the newest "post" in the chain)
+  	"22-newsletter-3.html",  
+	"21-Acrylic_painting_woman_in_hat.html",
+	"20-Comics_pre-production_assignment.html",
+	"19-painting_class_woman_in_bar.html",
+  	"18-painting_class_still_life.html",
+	"17-newsletter-2.html",
+	"16-croquis.html",
+	"15-acrylics-on-wall.html",
+	"14-portrait-acrylics-woman.html",
+	"13-boat_on_lake.html",
+	"12-portrait-watercolor.html",
+	"11-pencil-ink-Pecsi.html",
+	"10-pencil-ink-watercolor-stromholm.html",
+	"9-watercolor_face_shape.html",
+	"8-acrylics_close_up_portrait.html",
+	"7-sense.html",
+	"6-hope.html",
+	"5-pencil_drawing_woman.html",
+	"4-watercolor_weekend.html",
+	"3-newsletter-1.html",
+	"2-summer_books_2024.html"];
+// Each item is just the filename. You’ll add a new line at the top when posting something new.
