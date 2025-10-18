658bc8b831330258183575989bcdccdbbb153b5f
JanSandstorm <137768173+JanSandstorm@users.noreply.github.com>
2025-10-18 17:50:36 +0200

    Add files via upload

---
+++ letters.js
@@ -0,0 +1,9 @@
+// posts.js — a shared list of all your pages in reverse chronological order
+const posts = [
+  	"index.html",              // The landing page (acts as the newest "post" in the chain)
+  	"newsletters.html",              // The landing page (acts as the newest "post" in the chain)
+  	"22-newsletter-3.html",  
+	"17-newsletter-2.html",
+	"3-newsletter-1.html"];
+// Each item is just the filename. You’ll add a new line at the top when posting something new.
+//
---
+++ projects.js
@@ -0,0 +1,6 @@
+// posts.js — a shared list of all your pages in reverse chronological order
+const posts = [
+  	"index.html",              // The landing page (acts as the newest "post" in the chain)
+	"projects.html"];
+// Each item is just the filename. You’ll add a new line at the top when posting something new.
+//
