6ac8b56d4007ef0fe42633c1efb5b7780a354f43
JanSandstorm <137768173+JanSandstorm@users.noreply.github.com>
2025-10-29 11:24:43 +0100

    Add files via upload

--- projects.js
+++ projects.js
@@ -1,6 +1,7 @@
 // posts.js — a shared list of all your pages in reverse chronological order
 const posts = [
   	"index.html",              // The landing page (acts as the newest "post" in the chain)
-	"projects.html"];
+	"projects.html",
+	"24-comics_project_little_eagle_down.html"];
 // Each item is just the filename. You’ll add a new line at the top when posting something new.
 //
